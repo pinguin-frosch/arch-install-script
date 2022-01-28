e5d446e06740c35d94139583a010e23d7ce83faf
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-01-28 15:49:10 +0000

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#1. Software esencial
+#6. Software esencial
 pacman -S --noconfirm grub efibootmgr os-prober vim stow plasma dolphin alacritty base-devel ark gnome-keyring gwenview ntfs-3g nvidia nvidia-prime obs-studio okular partitionmanager spectacle virtualbox virtualbox-guest-iso virtualbox-host-modules-arch vlc zsh zsh-completions networkmanager
 
-#2. Software opcional
-echo -n "¿Instalar software opcional? [s/n]: "
-read b
-if [[ $b == "s" ]]; then
-    pacman -S --noconfirm kde-cli-tools ffmpegthumbs kdegraphics-thumbnailers p7zip unrar unarchiver lzop lrzip qt5-imageformats kimageformats ebook-tools kdegraphics-mobipocket libzip calligra
-fi
+#7. Software opcional
+# echo -n "¿Instalar software opcional? [s/n]: "
+# read b
+# if [[ $b == "s" ]]; then
+pacman -S --noconfirm ffmpegthumbs kdegraphics-thumbnailers p7zip unrar unarchiver qt5-imageformats kimageformats kdegraphics-mobipocket
+# fi
 
-#3. Configuración básica
+#8. Configuración básica
 ln -sf /usr/share/zoneinfo/America/Santiago /etc/localtime
 hwclock --systohc
-echo "es_CL.UTF-8 UTF-8" >> /etc/locale.gen
+# echo "es_CL.UTF-8 UTF-8" >> /etc/locale.gen
+sed -i "s/^#es_CL.UTF-8 UTF-8/es_CL.UTF-8 UTF-8/" /etc/locale.gen
 locale-gen
 echo "LANG=es_CL.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
@@ -21,10 +22,10 @@ echo -n "Hostname: "
 read hostname
 echo "$hostname" >> /etc/hostname
 
-#4. Configuración sudo
+#9. Configuración sudo
 sed -i "s/^# %wheel ALL=(ALL) ALL.*/%wheel ALL=(ALL) ALL/" /etc/sudoers
 
-#5. Instalación y configuración grub
+#10. Instalación y configuración grub
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 sed -i "s/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/" /etc/default/grub
 sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/" /etc/default/grub
@@ -33,15 +34,15 @@ sed -i "s/^#GRUB_SAVEDEFAULT=.*/GRUB_SAVEDEFAULT=true/" /etc/default/grub
 sed -i "s/^#GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=false/" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-#6. Servicios
+#11. Servicios
 systemctl enable NetworkManager
 systemctl enable sddm
 
-#7. Contraseña root
+#12. Contraseña root
 echo "Constraseña root"
 passwd
 
-#8. Creación y configuración usuario
+#13. Creación y configuración usuario
 echo -n "Nombre de usuario: "
 read username
 useradd -m $username
@@ -49,10 +50,6 @@ echo "Constraseña de usuario"
 passwd $username
 usermod -aG wheel $username
 usermod -s /usr/bin/zsh $username
-echo -n "Comentario de usuario: "
-read $comment
-usermod -c "$comment" $username
 
-#9. Reiniciar
-exit
-reboot
+#14. Salir
+# exit
