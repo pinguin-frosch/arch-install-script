647656d29adabea67ae647c7e80b3ce347a32630
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-01-28 20:52:37 -0300

    No hace falta la parte 3, mejor configuro la tablet manualmente

--- install-3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Opentablerdriver
-systemctl --user enable --now opentabletdriver.service
-echo "blacklist hid_uclogic" >> /etc/modprobe.d/blacklist.conf
-rmmod hid_uclogic
-
-# Eliminar
-rm -rf ./install-3.sh
