7051766f175250707e36e1e3acaa9e2a0e42445d
Gabriel Barrientos <gabri12.rubik@gmail.com>
2026-04-04 17:27:14 -0300

    Use a custom kw function to call kwriteconfig6 more easily

--- install-3.sh
+++ install-3.sh
@@ -11,61 +11,88 @@ dinitctl enable wireplumber
 # Install aur packages
 yay -S --noconfirm --needed - < $HOME/Programming/aur/aur.txt
 
+# Custom function to write configs much easier
+kw() {
+    local file="" key="" val="" type=""
+    local groups=()
+
+    # parse named arguments
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            -f|--file)  file="$2"; shift 2 ;;
+            -k|--key)   key="$2";  shift 2 ;;
+            -v|--value) val="$2";  shift 2 ;;
+            -t|--type)  type="$2"; shift 2 ;;
+            -g|--group) groups+=("--group" "$2"); shift 2 ;;
+            *) echo "Unknown parameter: $1"; return 1 ;;
+        esac
+    done
+
+    # create the command
+    local cmd=(kwriteconfig6 --notify --file "$file" "${groups[@]}" --key "$key" "$val")
+
+    # add the type if it exists
+    [[ -n "$type" ]] && cmd+=(--type "$type")
+
+    # run the command
+    "${cmd[@]}"
+}
+
 # Apply some kde plasma settings
 # kcminputrc
-kwriteconfig6 --file="kcminputrc" --group="Keyboard" --key="NumLock" 0
-kwriteconfig6 --file="kcminputrc" --group="Keyboard" --key="RepeatDelay" 300
-kwriteconfig6 --file="kcminputrc" --group="Keyboard" --key="RepeatRate" 50
+kw -f kcminputrc -g Keyboard -k NumLock -v 0
+kw -f kcminputrc -g Keyboard -k RepeatDelay -v 300
+kw -f kcminputrc -g Keyboard -k RepeatRate -v 50
 
 # kxkbrc
-kwriteconfig6 --file="kxkbrc" --group="Layout" --key="LayoutList" "pro"
-kwriteconfig6 --file="kxkbrc" --group="Layout" --key="Options" "caps:swapescape"
-kwriteconfig6 --file="kxkbrc" --group="Layout" --key="ResetOldOptions" --type=bool true
-kwriteconfig6 --file="kxkbrc" --group="Layout" --key="Use" --type=bool true
+kw -f kxkbrc -g Layout -k LayoutList -v pro
+kw -f kxkbrc -g Layout -k Options -v caps:swapescape
+kw -f kxkbrc -g Layout -k ResetOldOptions -v true -t bool
+kw -f kxkbrc -g Layout -k Use -v true -t bool
 
 # kwinrc
-kwriteconfig6 --file="kwinrc" --group="Effect-overview" --key="BorderActivate" 9
-kwriteconfig6 --file="kwinrc" --group="Plugins" --key="shakecursorEnabled" --type="bool" false
+kw -f kwinrc -g Effect-overview -k BorderActivate -v 9
+kw -f kwinrc -g Plugins -k shakecursorEnabled -v false -t bool
 
 # kdeglobals
-kwriteconfig6 --file="kdeglobals" --group="KDE" --key="AnimationDurationFactor" 0
-kwriteconfig6 --file="kdeglobals" --group="KDE" --key="SingleClick" --type="bool" true
+kw -f kdeglobals -g KDE -k AnimationDurationFactor -v 0
+kw -f kdeglobals -g KDE -k SingleClick -v true -t bool
 
 # krunnerrc
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="baloosearchEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="browserhistoryEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="browsertabsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="helprunnerEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_appstreamEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_bookmarksrunnerEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_charrunnerEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_colorsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_dictionaryEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_katesessionsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_killEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_konsoleprofilesEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_kwinEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_placesrunnerEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_plasma-desktopEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_powerdevilEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_recentdocumentsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_sessionsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_spellcheckEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_systemsettingsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="krunner_webshortcutsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="locationsEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="org.kde.activities2Enabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="org.kde.datetimeEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="unitconverterEnabled" --type="bool" false
-kwriteconfig6 --file="krunnerrc" --group="Plugins" --key="windowsEnabled" --type="bool" false
+kw -f krunnerrc -g Plugins -k baloosearchEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k browserhistoryEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k browsertabsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k helprunnerEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_appstreamEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_bookmarksrunnerEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_charrunnerEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_colorsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_dictionaryEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_katesessionsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_killEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_konsoleprofilesEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_kwinEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_placesrunnerEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_plasma-desktopEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_powerdevilEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_recentdocumentsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_sessionsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_spellcheckEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_systemsettingsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k krunner_webshortcutsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k locationsEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k org.kde.activities2Enabled -v false -t bool
+kw -f krunnerrc -g Plugins -k org.kde.datetimeEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k unitconverterEnabled -v false -t bool
+kw -f krunnerrc -g Plugins -k windowsEnabled -v false -t bool
 
 # baloofilerc
-kwriteconfig6 --file="baloofilerc" --group="General" --key="only basic indexing" --type="bool" true
+kw -f baloofilerc -g General -k "only basic indexing" -v true -t bool
 
 # powerdevilrc
-kwriteconfig6 --file="powerdevilrc" --group="Battery" --group="SuspendAndShutdown" --key="SleepMode" 3
-kwriteconfig6 --file="powerdevilrc" --group="LowBattery" --group="SuspendAndShutdown" --key="SleepMode" 3
+kw -f powerdevilrc -g Battery -g SuspendAndShutdown -k SleepMode -v 3
+kw -f powerdevilrc -g LowBattery -g SuspendAndShutdown -k SleepMode -v 3
 
 # dolphinrc
-kwriteconfig6 --file="dolphinrc" --group="General" --key="RememberOpenedTabs" --type="bool" false
-kwriteconfig6 --file="dolphinrc" --group="VersionControl" --key="enabledPlugins" "Git"
+kw -f dolphinrc -g General -k RememberOpenedTabs -v false -t bool
+kw -f dolphinrc -g VersionControl -k enabledPlugins -v Git
